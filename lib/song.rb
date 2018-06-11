9aec0bb3021e8b16e4481f95f86c6b666f51694d
randihi <randihigashi@gmail.com>
2018-06-11 15:01:07 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,28 +1,28 @@
 class Song
-  attr_accessor :name, :artist, :genre
+  attr_accessor :name, :artist, :genre #getter methods
 
-  @@count = 0
-  @@artists = []
-  @@genres = []
+  @@count = 0 
+  @@artists = [] #array
+  @@genres = [] #array
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
-    @@genres << genre
-    @@artists << artist
+    @@genres << genre #adds genre to genre array
+    @@artists << artist #adds artist t artist array
   end
 
-  def self.count
+  def self.count #Class method
     @@count
   end
 
-  def self.artists
+  def self.artists #Class method
     @@artists.uniq
   end
 
-  def self.artist_count
+  def self.artist_count #Class method
     artist_count = {}
     @@artists.each do |artist|
       if artist_count[artist]
@@ -34,11 +34,11 @@ class Song
     artist_count
   end
 
-  def self.genres
+  def self.genres #Class method
     @@genres.uniq
   end
 
-  def self.genre_count
+  def self.genre_count #Class method
     genre_count = {}
     @@genres.each do |genre|
       if genre_count[genre]
